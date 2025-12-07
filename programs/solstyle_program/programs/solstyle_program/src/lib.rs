3816750917e679a881c941afe88951844f76ad11
Vedansh555 <officialvedanshsingh@gmail.com>
2025-12-07 04:16:02 +0000

    Day 3: Completed buy_drop logic with commission split and Lamport transfers

--- programs/solstyle_program/programs/solstyle_program/src/lib.rs
+++ programs/solstyle_program/programs/solstyle_program/src/lib.rs
@@ -24,6 +24,52 @@ pub mod solstyle_program {
 
         Ok(())
     }
+   
+
+    pub fn buy_drop(
+        ctx: Context<BuyDrop>,
+        amount_lamports: u64, 
+    ) -> Result<()> {
+        let drop = &ctx.accounts.drop;
+        let commission_bps = drop.commission_bps;
+
+        require_eq!(amount_lamports, drop.price, ErrorCode::IncorrectPaymentAmount);
+
+       
+        let commission_lamports = (amount_lamports * commission_bps as u64) / 10000;
+        
+  
+        let seller_lamports = amount_lamports.checked_sub(commission_lamports).unwrap();
+
+       
+        anchor_lang::system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                anchor_lang::system_program::Transfer {
+                    from: ctx.accounts.buyer.to_account_info(),
+                    to: ctx.accounts.seller.to_account_info(),
+                },
+            ),
+            seller_lamports,
+        )?;
+
+        
+        anchor_lang::system_program::transfer(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                anchor_lang::system_program::Transfer {
+                    from: ctx.accounts.buyer.to_account_info(),
+                    to: ctx.accounts.commission_recipient.to_account_info(),
+                },
+            ),
+            commission_lamports,
+        )?;
+
+
+        Ok(())
+    }
+    
+    
 }
 
 #[derive(Accounts)]
@@ -38,6 +84,24 @@ pub struct CreateDrop<'info> {
     #[account(mut)]
     pub seller: Signer<'info>,
 
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct BuyDrop<'info>{
+    #[account(
+        has_one = seller,
+        mut
+    )]
+    pub drop: Account<'info, Drop>,
+    #[account(mut)]
+    pub buyer: Signer<'info>,
+    #[account(mut)]
+    /// CHECK: The seller key is verified via the drop.has_one check above.
+    pub seller: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: This is a fixed, known platform wallet for receiving commissions.
+    pub commission_recipient: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 }
 
@@ -61,4 +125,6 @@ impl Drop {
 pub enum ErrorCode {
     #[msg("Commission basis points must be <= 10000")]
     InvalidCommissionBps,
+    #[msg("The amount of SOL sent does not match the required drop price.")]
+    IncorrectPaymentAmount,
 }
